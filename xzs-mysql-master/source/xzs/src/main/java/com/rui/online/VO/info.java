d217ebb81dd7ad5e128cbd039e2e11f431e052b1
LdLtd <503834595@qq.com>
2022-11-21 13:05:21 +0800

    1121

---
+++ xzs-mysql-master/source/xzs/src/main/java/com/rui/online/VO/info.java
@@ -0,0 +1,11 @@
+package com.rui.online.VO;
+
+/**
+ * @Date : 2022/11/11
+ * @Description:
+ */
+public class info {
+    public static void main(String[] args) {
+        System.out.println("Name:陆丹\nSex:女\nClass:21智科1班\nNumber:2021900408");
+    }
+}
