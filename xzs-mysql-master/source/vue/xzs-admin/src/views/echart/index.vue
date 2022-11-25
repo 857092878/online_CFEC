f3e34a62dc6814912d2143847e5e42b1f7cb95a1
LdLtd <503834595@qq.com>
2022-11-25 19:24:47 +0800

    .

---
+++ xzs-mysql-master/source/vue/xzs-admin/src/views/echart/index.vue
@@ -0,0 +1,33 @@
+<template>
+   <div>
+   <iframe src="http://localhost:8000/index.html"  id="mobsf"  scrolling="no" frameborder="0" style="position:relative;top:0px;left: -50px;"></iframe>
+   
+   </div>
+   </template>
+   
+   <script>
+   export default {
+   data () {
+   return {
+   }
+   },
+   mounted(){
+   /**
+   * iframe-宽高自适应显示
+   */
+   function changeMobsfIframe(){
+   const mobsf = document.getElementById('mobsf');
+   const deviceWidth = document.body.clientWidth;
+   const deviceHeight = document.body.clientHeight;
+   mobsf.style.width = (Number(deviceWidth)-114) + 'px'; 
+   mobsf.style.height = (Number(deviceHeight)-78) + 'px'; 
+   }
+   
+   changeMobsfIframe()
+   
+   window.onresize = function(){
+   changeMobsfIframe()
+   }
+   },
+   }
+   </script>
